30d2ee96b74816beac6da7c3c8de13ea34246387
pradxyz <ypradyumna89@gmail.com>
2025-06-09 06:46:35 +0000

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a=2;
+    	int b=3;
+    	int c=a+b;
+    	System.out.println("2 + 3 = "+c);
+        System.out.println( "successfully Executed!" );
+    }
+}
